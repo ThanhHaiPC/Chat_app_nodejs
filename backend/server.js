9060d0b1229beea63cec270945fb00653483a2ef
ThanhHaiPC <ThanhHaiPC2002@gmail.com>
2024-04-17 20:09:31 +0700

    Deloyment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 
-const app = express();
 const PORT =process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,7 +27,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
   });
@@ -40,7 +51,7 @@ app.listen(PORT, () => {
 
 // const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
+// const __dirname = path.resolve()vscode-file://vscode-app/c:/Users/Administrator/AppData/Local/Programs/Microsoft%20VS%20Code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html;
 
 // dotenv.config();
 
