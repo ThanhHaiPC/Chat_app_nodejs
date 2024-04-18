4d8ee1319215caca4206fb83238e8975760efe06
luung <luunguyentrungtri@gmail.com>
2024-04-18 17:45:48 +0700

    Fix Register

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -18,11 +18,7 @@ const Messages = () => {
 
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
-			{/* {loading && [...Array(3)].map((_,idx)=> <MessageSkeleton key ={idx}/>)}
-
-			{!loading && messages.length === 0 && (
-				<p className='text-center'>Send a message to start the conversation</p>
-			)}  */}
+			
 			{!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
